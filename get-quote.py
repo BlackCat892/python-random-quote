62df2aeebb049df0a9c5499006d2f3d80c29c515
Marija Kostovska <marijakostovska8@outlook.com>
2020-04-05 20:14:00 +0200

    All tried

--- get-quote.py
+++ get-quote.py
@@ -4,13 +4,18 @@ import random
 def primary():
    #print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
 
-  last = len(quotes) - 1
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+    last = len(quotes) - 1
+    rnd = random.randint(0, last)
+    print(quotes[rnd]*3)
 
-if __name__== "__main__":
-  primary()
+   # f = open("quotes.txt", "a+")
+   # f.write("The clearest way into the Universe is through a forest wilderness.\n")
+    #f.write("The mountains are calling and I must go.\n")
+    #f.close()
+
+if __name__ == "__main__":
+    primary()
